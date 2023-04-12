63c5abda07c246b5bf242418b4d5b1742aeff022
JC Hipolito <jchips12@gmail.com>
2023-04-12 15:49:38 -0400

    use function to get numbers greater than 5

--- problem_04.py
+++ problem_04.py
@@ -1,13 +1,16 @@
-myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-myNumbersGt5 = []
+def get_numbers_gt_5(myNumbers):
+    my_numbers_gt_5 = []
+    for num in myNumbers:
+        if num > 5:
+            my_numbers_gt_5.append(num)
+    return my_numbers_gt_5
+
+
+my_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_numbers_gt_5 = get_numbers_gt_5(my_numbers)
 
 print("List of numbers:")
-print(myNumbers)
-
-for num in myNumbers:
-    if num > 5:
-        myNumbersGt5.append(num)
+print(my_numbers)
 
 print("List of numbers that are larger than 5:")
-print(myNumbersGt5)
-
+print(my_numbers_gt_5)
